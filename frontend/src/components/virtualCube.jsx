ac02ec678f743455b08ce0f5541ab4ba9b19eae8
_jATMOS <bks.mhryadayada@gmail.com>
2025-01-14 23:04:08 +0545

    feat: working scramble

--- frontend/src/components/virtualCube.jsx
+++ frontend/src/components/virtualCube.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -9,6 +9,7 @@ import {
   Roll,
   stateMap,
 } from "../assets/utils/3Dhelpers";
+import { moveNotationTo3d } from "../assets/utils/3Dhelpers";
 
 const VirtualCube = () => {
   const mountRef = useRef(null);
@@ -23,6 +24,7 @@ const VirtualCube = () => {
   const resizeObserverRef = useRef(null);
   const location = useLocation();
   const cubeColors = location.state?.cubeColors || initialColors;
+  const [scrambling, setScramblingState] = useState(false);
 
   const cleanColors = (cubeColors) => {
     const cleanedCubeColors = {};
@@ -59,7 +61,7 @@ const VirtualCube = () => {
 
     // Scene setup
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color();
+    scene.background = new THREE.Color(0x000000);
     sceneRef.current = scene;
 
     // Renderer setup
@@ -212,14 +214,22 @@ const VirtualCube = () => {
       });
     };
   }, []);
-
+  const scramble = () => {
+    for (let i = 0; i <= 20; i++) {
+      const keys = Object.keys(moveNotationTo3d);
+      const randomIndex = Math.floor(Math.random() * keys.length);
+      const randomObject = moveNotationTo3d[keys[randomIndex]];
+      console.log(randomObject);
+      moves(randomObject.position, randomObject.direction);
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row md:h-screen w-full pt-16 ">
       <div ref={mountRef} className="w-full md:w-2/3 h-[60vh] md:h-full" />
-      <div className="w-full md:w-1/3 p-4 bg-gray-900">
-        <div className="grid grid-cols-3 gap-4 p-4 bg-black bg-opacity-50 rounded-lg">
+      <div className="w-full md:w-1/3 p-4 bg-gray-900 rounded-l-3xl">
+        <div className="grid grid-cols-3 gap-4 p-4 bg-black bg-opacity-50 rounded-3xl">
           <button
-            className="h-12 text-lg font-semibold text-white bg-gray-800 rounded hover:bg-gray-700 active:bg-gray-900"
+            className="h-12 text-lg font-semibold text-white bg-gray-800 rounded hover:bg-gray-700 active:bg-gray-900 "
             onClick={() => moves("front", 1)}
           >
             F'
@@ -315,6 +325,14 @@ const VirtualCube = () => {
             onClick={() => moves("rotateLeft", -1)}
           >
             Left
+          </button>
+        </div>
+        <div className="mt-4 p-4 bg-black bg-opacity-50 rounded-lg flex justify-center ">
+          <button
+            className="h-12 text-lg p-4 font-semibold flex items-center justify-center text-white bg-gray-800 rounded hover:bg-gray-700 active:bg-gray-900"
+            onClick={scramble}
+          >
+            Scramble
           </button>
         </div>
       </div>
